e14437de9ead5edc1bc4f8ffb982833c8234f810
wenxingjie <26127631+haishangfeie@users.noreply.github.com>
2023-06-28 21:08:15 +0800

    feat: 只有对应字段被修改时触发响应式

--- reactive.js
+++ reactive.js
@@ -1,46 +1,57 @@
-// 目标:obj.text改变时，副作用函数可以自动执行
-
-// const obj = {
-//   text: 'hello world',
-// };
-// function effect() {
-//   document.body.innerText = obj.text;
-// }
-// effect()
-// setTimeout(() => {
-//   obj.text = 'hello vue';
-// }, 1000);
-
 /**
- * 当前effect是硬编码实现的响应式
- * 现在希望effect可以不是硬编码
+ * 希望只在对应属性改变时才触发响应式
  */
 let activeEffect;
 function effect(fn) {
   activeEffect = fn;
   fn();
+  activeEffect = null;
 }
-const bucket = new Set();
+const bucket = new WeakMap();
 const data = {
   text: 'hello world',
+  text2: 'text2',
 };
 const obj = new Proxy(data, {
   get(target, key) {
-    bucket.add(activeEffect);
+    if (!activeEffect) {
+      return target[key];
+    }
+    if (!bucket.has(target)) {
+      bucket.set(target, new Map());
+    }
+    const targetMap = bucket.get(target);
+    if (!targetMap.has(key)) {
+      targetMap.set(key, new Set());
+    }
+    const deps = targetMap.get(key);
+    deps.add(activeEffect);
     return target[key];
   },
   set(target, key, newVal) {
     target[key] = newVal;
-    bucket.forEach((fn) => fn());
-    return true;
+    const targetMap = bucket.get(target);
+    if (!targetMap) {
+      return;
+    }
+    const deps = targetMap.get(key);
+    if (!deps) {
+      return;
+    }
+    deps.forEach((fn) => fn());
   },
 });
 
 // 执行副作用函数，触发getter
 effect(() => {
-  console.log('--effect--');
+  console.log('--effect1--');
   document.body.innerText = obj.text;
+});
+effect(() => {
+  console.log('--effect2--');
+  console.log('obj.text2', obj.text2);
 });
 setTimeout(() => {
   obj.text = 'hello vue';
+  // obj.other = 'hhh';
 }, 1000);
