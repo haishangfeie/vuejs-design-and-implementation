f5d58a4c6e6873d7978429e7dad2867d93e1da3a
wenxingjie <26127631+haishangfeie@users.noreply.github.com>
2023-06-29 21:52:48 +0800

    feat: 记录一个发现的问题（不清楚怎么解决）

--- reactive.js
+++ reactive.js
@@ -83,18 +83,19 @@ function trigger(target, key) {
 //   }, 1000);
 // }, 1000);
 
-effect(() => {
-  console.log('--effect1');
-  effect(() => {
-    console.log('--effect2');
-    console.log('---obj.text2', obj.text2);
-  });
-  console.log('---obj.text', obj.text);
-});
-setTimeout(() => {
-  console.log('1秒后');
-  obj.text = 'hhh';
-}, 1000);
+// --------
+// effect(() => {
+//   console.log('--effect1');
+//   effect(() => {
+//     console.log('--effect2');
+//     console.log('---obj.text2', obj.text2);
+//   });
+//   console.log('---obj.text', obj.text);
+// });
+// setTimeout(() => {
+//   console.log('1秒后');
+//   obj.text = 'hhh';
+// }, 1000);
 /**
  * 不支持嵌套时的结果：
  * --effect1
@@ -114,3 +115,25 @@ setTimeout(() => {
  * ---obj.text2 text2
  * ---obj.text hhh
  */
+
+// --------
+/**
+ * 但是上面的实现虽然实现了栈，但是也意味着修改obj.text2时会有2个副作用函数执行
+ * 是不是需要将原来的副作用函数卸载掉呢？
+ */
+// effect(() => {
+//   console.log('--effect1');
+//   effect(() => {
+//     console.log('--effect2');
+//     console.log('---obj.text2', obj.text2);
+//   });
+//   console.log('---obj.text', obj.text);
+// });
+// setTimeout(() => {
+//   console.log('1秒后');
+//   obj.text = 'hhh';
+//   setTimeout(() => {
+//     console.log('又1秒后');
+//     obj.text2 = '2222';
+//   }, 1000);
+// }, 1000);
