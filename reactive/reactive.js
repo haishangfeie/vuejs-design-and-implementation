dea0ed92b3ce2fd4aedbb6490f89811ae3e7ae1f
wenxingjie <26127631+haishangfeie@users.noreply.github.com>
2023-08-23 00:08:58 +0800

    feat: 实现shallowReactive支持浅响应

--- reactive/reactive.js
+++ reactive/reactive.js
@@ -94,62 +94,12 @@ function trigger(target, key, type) {
   });
 }
 
-export const reactive = (data) => {
-  return new Proxy(data, {
-    get(target, key, receiver) {
-      // 为了让代理对象可以访问到原始数据
-      if (key === 'raw') {
-        return target;
-      }
-      track(target, key);
-      const res = Reflect.get(target, key, receiver);
+export const reactive = (obj) => {
+  return createReactive(obj, false);
+};
 
-      if (typeof res === 'object' && res !== null) {
-        return reactive(res);
-      }
-      return res;
-    },
-    set(target, key, newVal, receiver) {
-      const type = Object.prototype.hasOwnProperty.call(target, key)
-        ? TriggerTypes.SET
-        : TriggerTypes.ADD;
-      const oldVal = target[key];
-      const res = Reflect.set(target, key, newVal, receiver);
-
-      // 确保receiver是target的代理对象
-      if (target === receiver.raw) {
-        // 只有值发生变化，触发触发响应
-        // 新值与旧值不全等，并且不都是NaN时才触发响应
-        if (newVal !== oldVal && (oldVal === oldVal || newVal === newVal)) {
-          trigger(target, key, type);
-        }
-      }
-
-      return res;
-    },
-    // 为了在副作用函数内使用in操作符时可以触发依赖收集
-    // 这里的原理大概是这样的：in的底层会调用HasProperty的抽象方法
-    // 而HasProperty会调用对象内部的[[HasProperty]]，它对应的拦截函数正是has
-    has(target, key) {
-      track(target, key);
-      return Reflect.has(target, key);
-    },
-    // 拦截 for in，进行依赖收集
-    ownKeys(target) {
-      track(target, ITERATE_KEY);
-      return Reflect.ownKeys(target);
-    },
-    // 拦截属性删除操作
-    deleteProperty(target, key) {
-      const hasKey = Object.prototype.hasOwnProperty.call(target, key);
-      const res = Reflect.deleteProperty(target, key);
-      // 存在key而且删除成功
-      if (hasKey && res) {
-        trigger(target, key, TriggerTypes.DELETE);
-      }
-      return res;
-    },
-  });
+export const shallowReactive = (obj) => {
+  return createReactive(obj, true);
 };
 
 // 计算属性
@@ -250,4 +200,62 @@ function traverse(obj, seen = new Set()) {
 
 function isObject(obj) {
   return obj !== null && typeof obj === 'object';
+}
+
+function createReactive(data, isShallow = false) {
+  return new Proxy(data, {
+    get(target, key, receiver) {
+      // 为了让代理对象可以访问到原始数据
+      if (key === 'raw') {
+        return target;
+      }
+      track(target, key);
+      const res = Reflect.get(target, key, receiver);
+
+      if (typeof res === 'object' && res !== null && !isShallow) {
+        return reactive(res);
+      }
+      return res;
+    },
+    set(target, key, newVal, receiver) {
+      const type = Object.prototype.hasOwnProperty.call(target, key)
+        ? TriggerTypes.SET
+        : TriggerTypes.ADD;
+      const oldVal = target[key];
+      const res = Reflect.set(target, key, newVal, receiver);
+
+      // 确保receiver是target的代理对象
+      if (target === receiver.raw) {
+        // 只有值发生变化，触发触发响应
+        // 新值与旧值不全等，并且不都是NaN时才触发响应
+        if (newVal !== oldVal && (oldVal === oldVal || newVal === newVal)) {
+          trigger(target, key, type);
+        }
+      }
+
+      return res;
+    },
+    // 为了在副作用函数内使用in操作符时可以触发依赖收集
+    // 这里的原理大概是这样的：in的底层会调用HasProperty的抽象方法
+    // 而HasProperty会调用对象内部的[[HasProperty]]，它对应的拦截函数正是has
+    has(target, key) {
+      track(target, key);
+      return Reflect.has(target, key);
+    },
+    // 拦截 for in，进行依赖收集
+    ownKeys(target) {
+      track(target, ITERATE_KEY);
+      return Reflect.ownKeys(target);
+    },
+    // 拦截属性删除操作
+    deleteProperty(target, key) {
+      const hasKey = Object.prototype.hasOwnProperty.call(target, key);
+      const res = Reflect.deleteProperty(target, key);
+      // 存在key而且删除成功
+      if (hasKey && res) {
+        trigger(target, key, TriggerTypes.DELETE);
+      }
+      return res;
+    },
+  });
 }
